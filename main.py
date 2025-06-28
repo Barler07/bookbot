d45c2f15b06d9d4164f8cc89d7750073b7ea7b6e
barler07 <carson.miller115@gmail.com>
2025-06-28 15:46:52 -0600

    final

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ from stats import (
 
 
 def main():
-    book_path = sys.argv(1)
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
     print_report(book_path, num_words, chars_sorted_list)
-    if sys.argv(1) = None:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
     
 
 
